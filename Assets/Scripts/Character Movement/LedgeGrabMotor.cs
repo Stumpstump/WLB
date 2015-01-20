13c19806aba6bb40fa2123733aa5e4d4d0dbaefa
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-01-20 18:12:33 -0500

    initial commit to my own branch
    all the project files that i have to date

---
+++ Assets/Scripts/Character Movement/LedgeGrabMotor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WLB
+{
+	public class LedgeGrabMotor : MonoBehaviour 
+	{
+		public float climbTime;
+
+		public Transform playerPos;
+		public Transform climbPos;
+
+		public Collider2D spaceCheck;
+		public Collider2D ledgeCheck;
+
+		public List<GameObject> gosInSpaceCheck = new List<GameObject>();
+		public bool isLedge = false;
+		public bool spaceEmpty = false;
+
+		private bool isClimbing;
+
+		private void Update()
+		{
+			Debug.Log ("isLedge " + isLedge + " spaceEmpty " + spaceEmpty);
+			if(isLedge && spaceEmpty && !isClimbing)
+			{
+				StartCoroutine(Climb());
+			}
+		}
+
+
+		private IEnumerator Climb()
+		{
+			isClimbing = true;
+
+			Vector2 endPos = climbPos.position;
+			Vector2 currentPos = playerPos.position;
+			Debug.Log ("climbing");
+			while(currentPos != endPos)
+			{
+				yield return null;
+				Vector2.Lerp(currentPos, endPos, climbTime * Time.fixedDeltaTime);
+			}
+
+			isClimbing = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Character Movement/SpaceCheck.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WLB
+{
+	public class SpaceCheck : MonoBehaviour 
+	{
+		public LedgeGrabMotor ledgeGrabMotor;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			List<GameObject> gos = ledgeGrabMotor.gosInSpaceCheck;
+
+			foreach(GameObject go in gos)
+			{
+				if(go == other.gameObject)
+				{
+					return;
+				}
+				else
+				{
+					gos.Add(go);
+					ledgeGrabMotor.gosInSpaceCheck = gos;
+				}
+			}
+	
+			ledgeGrabMotor.spaceEmpty = gos.Count == 0;
+		}
+
+
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			List<GameObject> gos = ledgeGrabMotor.gosInSpaceCheck;
+
+			foreach(GameObject go in gos)
+			{
+				if(go == other.gameObject)
+				{
+					gos.Remove(go);
+					break;
+				}
+			}
+
+			ledgeGrabMotor.spaceEmpty = gos.Count == 0;
+		}
+	}
+}
