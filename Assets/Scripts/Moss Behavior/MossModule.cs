ccb4aadcd8a7a8ce69e90ee1bb83b64ae43b7cf2
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-02-03 18:42:11 -0500

    got the light behavior working a bit better

--- Assets/Scripts/Moss Behavior/MossModule.cs
+++ Assets/Scripts/Moss Behavior/MossModule.cs
@@ -22,15 +22,18 @@ namespace WLB
 				GameObject light = Instantiate(mossLightPrefab, lightPosition.position, Quaternion.identity) as GameObject;
 				holdingLight = true;
 				heldLight = light;
+				heldLight.rigidbody2D.gravityScale = 0;
 			}
 
 			if(Input.GetKeyUp(KeyCode.Q))
 			{
+				heldLight.rigidbody2D.gravityScale = 1;
 				holdingLight = false;
 			}
 
 			if(holdingLight)
 			{
+
 				heldLight.transform.position = lightPosition.position;
 			}
 		}
